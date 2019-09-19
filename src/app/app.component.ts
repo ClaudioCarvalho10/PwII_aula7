550dd48cf3224da2570888827d703080ae51509b
claudio <claudiojrcarvalho@hotmail.com>
2019-09-18 21:46:50 -0300

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,19 @@ import { Component } from '@angular/core';
   selector: 'has-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'nitroooooooooo poooooint domingooooooo';
-foto = "favicon.ico";
+  })
+
+  
+  export class AppComponent {
+  title = 'uhuhhhhhhhhhhuuuuuuuuuuuuuuuuuu ';
+  foto = "favicon.ico";
+
+  
+  exibir(){
+    alert(this.title);
+   
+    }
+
+
 
 }
